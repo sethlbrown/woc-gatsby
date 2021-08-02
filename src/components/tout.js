2ac873dbe21efaef8e33b60f89860c59e3ca532a
Seth Brown <sethbrown@Seths-MacBook-Air.local>
2021-08-02 17:47:21 -0600

    Minor changes to tout

--- src/components/tout.js
+++ src/components/tout.js
@@ -13,17 +13,29 @@ export default function Tout() {
           placeholder="blurred"
         />
         <div
-          className="absolute inset-0 bg-gray-800 mix-blend-multiply"
+          className="absolute inset-0 bg-gradient-to-r from-gray-900 to-gray-100 mix-blend-multiply"
           aria-hidden="true"
         />
       </div>
       <div className="max-w-7xl mx-auto relative py-12 px-4 sm:py-32 sm:px-6 lg:px-16">
-        <h1 className="w-11/12 leading-relaxed tracking-wider text-2xl font-semibold text-white sm:text-3xl lg:w-1/2 lg:text-4xl">
+        <h1
+          className="
+          w-11/12 
+          text-2xl 
+          font-semibold 
+          text-white 
+          sm:text-5xl 
+          sm:w-3/4 
+          lg:w-7/12
+          lg:leading-normal
+          "
+        >
           A bicycle shop where members can grow and learn.
         </h1>
-        <p className="mt-6 font-semibold text-md text-teal-500 max-w-3xl">
-          <div className="w-1/2 pb-4 border-t-4 border-white" />
-          Read Our Story <BsArrowRight className="inline" />
+        <p className="mt-6 font-semibold text-base sm:text-2xl text-teal-500 max-w-3xl">
+          <div className="w-1/2 pb-4 border-t-2 border-white" />
+          Read Our Story{' '}
+          <BsArrowRight className="inline text-2xl sm:text-4xl font-black" />
         </p>
       </div>
     </div>
